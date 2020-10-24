34789bb9faebf6c8cb559f17b3d8438a9bac0b1a
wainfotechnologies <72775989+wainfotechnologies@users.noreply.github.com>
2020-10-24 08:45:25 +0530

    Add project files.

---
+++ WebcamjsDemo/wwwroot/webcamjs/Configuration.js
@@ -0,0 +1,25 @@
+﻿function ready() {
+	if (document.readyState == 'complete') {
+		Webcam.set({
+			width: 320,
+			height: 240,
+			image_format: 'jpeg',
+			jpeg_quality: 90
+		});
+		try {
+			Webcam.attach('#camera');
+		} catch (e) {
+			alert(e);
+		}
+	}
+}
+
+function take_snapshot() {
+	// take snapshot and get image data
+	var data = null;
+	Webcam.snap(function (data_uri) {
+
+		data = data_uri;
+	});
+	return data;
+}
